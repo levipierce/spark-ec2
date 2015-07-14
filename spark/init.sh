a2dcff34761decd86cd33f44b88cce96adaf43f8
Ubuntu <levipierce@gmail.com>
2015-07-14 03:55:30 +0000

    added version 1.5

--- spark/init.sh
+++ spark/init.sh
@@ -115,7 +115,8 @@ else
       else
         #wget http://s3.amazonaws.com/spark-related-packages/spark-1.2.1-bin-cdh4.tgz
         #wget http://archive.cloudera.com/cdh5/cdh/5/spark-1.3.0-cdh5.4.1.tar.gz
-        wget http://s3.amazonaws.com/spark-related-packages/spark-1.3.1-bin-cdh4.tgz
+        #wget http://s3.amazonaws.com/spark-related-packages/spark-1.3.1-bin-cdh4.tgz
+        wget http://people.apache.org/~pwendell/spark-nightly/spark-master-bin/latest/spark-1.5.0-SNAPSHOT-bin-cdh4.tgz
       fi
       ;;
     *)
